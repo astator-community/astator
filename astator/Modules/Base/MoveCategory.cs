c0307884a5b2b898f369defbcd86d7faee04b855
yiszza <1104573225@qq.com>
2022-03-27 15:41:25 +0800

    add api: LayoutDump

---
+++ astator/Modules/Base/MoveCategory.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.ObjectModel;
+
+
+namespace astator.Modules.Base;
+
+
+
+public class MoveCategory
+{
+#nullable enable
+    public bool? AccessibilityFocused { get; set; }
+    public Core.Graphics.Rect? Bounds { get; set; }
+    public bool? Checkable { get; set; }
+    public bool? Checked { get; set; }
+    public string? ClassName { get; set; }
+    public bool? Clickable { get; set; }
+    public int? Column { get; set; }
+    public int? ColumnCount { get; set; }
+    public int? ColumnSpan { get; set; }
+    public bool? ContextClickable { get; set; }
+    public int? Depth { get; set; }
+    public string? Desc { get; set; }
+    public bool? Dismissable { get; set; }
+    public int? DrawingOrder { get; set; }
+    public bool? Editable { get; set; }
+    public bool? Enabled { get; set; }
+    public bool? Focused { get; set; }
+    public int? IndexInParent { get; set; }
+
+    private string? id;
+    public string? Id
+    {
+        get => this.id;
+        set
+        {
+            this.id = value?[(value.LastIndexOf("/") + 1)..];
+        }
+    }
+    public bool? LongClickable { get; set; }
+    public string? PackageName { get; set; }
+    public int? Row { get; set; }
+    public int? RowCount { get; set; }
+    public int? RowSpan { get; set; }
+    public bool? Scrollable { get; set; }
+    public bool? Selected { get; set; }
+    public string? Text { get; set; }
+    public bool? VisibleToUser { get; set; }
+    public string Title
+    {
+        get
+        {
+            if (this.Id == null)
+            {
+                return $"{ this.ClassName?[(this.ClassName.LastIndexOf(".") + 1)..] }";
+            }
+            return $"{ this.ClassName?[(this.ClassName.LastIndexOf(".") + 1)..] }  [id: { this.Id }]";
+
+        }
+    }
+    public ObservableCollection<MoveCategory>? Children { get; set; }
+#nullable disable
+}
--- astator/Modules/DebugService.cs
+++ astator/Modules/DebugService.cs
@@ -3,11 +3,15 @@ using Android.Content;
 using Android.Content.PM;
 using Android.OS;
 using Android.Runtime;
+using Android.Views.Accessibility;
 using AndroidX.Core.App;
 using AndroidX.Core.Graphics.Drawable;
+using astator.Core.Accessibility;
 using astator.Core.Graphics;
 using astator.Core.Script;
 using astator.Modules.Base;
+using Newtonsoft.Json;
+using System.Collections.ObjectModel;
 using System.IO.Compression;
 using System.Net;
 using System.Net.Sockets;
@@ -191,6 +195,45 @@ internal class DebugService : Service, IDisposable
                             {
                                 pack = Stick.MakePackData("screenShot_fail", "截图服务未开启!");
                             }
+                            await stream.WriteAsync(pack);
+                            break;
+                        }
+                        case "layoutDump":
+                        {
+                            byte[] pack;
+
+                            if (PermissionHelperer.CheckScreenCap() && PermissionHelperer.CheckAccessibility())
+                            {
+                                try
+                                {
+                                    var img = await ScreenCapturer.Instance.AcquireLatestBitmapOnCurrentOrientation();
+                                    var imgStream = new MemoryStream();
+                                    img.Compress(Android.Graphics.Bitmap.CompressFormat.Png, 100, imgStream);
+                                    var imgBytes = imgStream.ToArray();
+
+                                    var layoutInfo = LayoutDump();
+                                    string js = JsonConvert.SerializeObject(layoutInfo);
+                                    var jsBytes = Encoding.UTF8.GetBytes(js);
+
+                                    var size = 4 + imgBytes.Length + 4 + jsBytes.Length;
+                                    using var ms = new MemoryStream(size);
+                                    ms.WriteInt32(imgBytes.Length);
+                                    ms.Write(imgBytes);
+                                    ms.WriteInt32(jsBytes.Length);
+                                    ms.Write(jsBytes);
+                                    var bytes = ms.GetBuffer();
+
+                                    pack = Stick.MakePackData("LayoutDump_success", bytes);
+                                }
+                                catch (Exception)
+                                {
+                                    pack = Stick.MakePackData("LayoutDump_fail", "布局获取失败!");
+                                }
+                            }
+                            else
+                            {
+                                pack = Stick.MakePackData("LayoutDump_fail", "截图服务或无障碍服务未开启!");
+                            }
 
 
                             await stream.WriteAsync(pack);
@@ -214,6 +257,43 @@ internal class DebugService : Service, IDisposable
                 ScriptLogger.RemoveCallback(key);
             }
         }, cancelToken);
+    }
+
+    private static MoveCategory LayoutDump(AccessibilityNodeInfo node = null)
+    {
+        node ??= Automator.GetCurrentWindowRoot();
+        var layoutInfo = new MoveCategory
+        {
+            AccessibilityFocused = node.AccessibilityFocused,
+            Bounds = node.GetBounds(),
+            Checkable = node.Checkable,
+            Checked = node.Checked,
+            ClassName = node.ClassName,
+            Clickable = node.Clickable,
+            ContextClickable = node.ContextClickable,
+            Depth = node.GetDepth(),
+            Desc = node.ContentDescription,
+            Dismissable = node.Dismissable,
+            DrawingOrder = node.DrawingOrder,
+            Editable = node.Editable,
+            Enabled = node.Enabled,
+            Focused = node.Focused,
+            Id = node.GetId(),
+            LongClickable = node.LongClickable,
+            PackageName = node.PackageName,
+            Scrollable = node.Scrollable,
+            Selected = node.Selected,
+            Text = node.Text,
+            VisibleToUser = node.VisibleToUser
+        };
+
+        if (node.ChildCount > 0) layoutInfo.Children = new ObservableCollection<MoveCategory>();
+
+        for (var i = 0; i < node.ChildCount; i++)
+        {
+            layoutInfo.Children.Add(LayoutDump(node.GetChild(i)));
+        }
+        return layoutInfo;
     }
 
 
